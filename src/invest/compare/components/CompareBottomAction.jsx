e5b81c0e6657b4ac608f58c4b82335ca01d5b634
moriaty12 <moriaty12@gmail.com>
2026-05-22 00:01:47 +0900

    목업 만들기 화면 레이아웃 2 누락 파일 추가

---
+++ src/invest/compare/components/CompareBottomAction.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CompareBottomAction() {
+  return (
+    <div className="compare-bottom-action">
+      <button className="outline-btn">
+        + 상품 추가
+      </button>
+
+      <button className="primary-btn">
+        비교 리포트 보기
+      </button>
+    </div>
+  );
+}
